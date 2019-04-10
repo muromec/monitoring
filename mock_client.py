4d70ba0a15d830b557c5e29dbcd607f13b26ad1e
Illya Petrov <ilya.muromec@gmail.com>
2019-04-10 07:47:05 +0000

    start monitoring

--- mock_client.py
+++ mock_client.py
@@ -1,11 +1,46 @@
+import json
+import random
 import asyncio
 import aiohttp
+
+def single_param_generator(initial, lower_rand, higher_rand):
+    yield initial
+
+    step = initial
+    def next_step():
+        return step + (random.randint(lower_rand, higher_rand) / 100.0)
+
+    while True:
+        step = next_step()
+        yield step
+
+def param_generators(config):
+    generators = {
+        key: single_param_generator(*args)
+        for key, args in config.items()
+    }
+    while True:
+        params = {
+            key: next(generator_fn)
+            for key, generator_fn in generators.items()
+        }
+        yield params
+
+PARAM_STEPS_CONFIG = {
+    'ph': [7.0, -2, 0],
+    'color': [0, 0, 50],
+    'bitterness': [0, 0, 20],
+    'turbidity': [0, 0, 20],
+    'alcohol': [0, 0, 5],
+    'attenuation': [0, 0, 30],
+    'gravity': [1, 0, 1],
+}
 
 async def main():
     async with aiohttp.ClientSession() as session:
         async with session.ws_connect('http://localhost:5555/inlet') as ws:
-            for n in range(20):
-                await ws.send_str(str(n))
+            for param in param_generators(PARAM_STEPS_CONFIG):
+                await ws.send_str(json.dumps(param))
                 await asyncio.sleep(1.0)
 
 if __name__ == '__main__':
--- static/main.js
+++ static/main.js
@@ -1,15 +1,91 @@
-function main() {
+function socket(dispatch) {
   const webSocket = new WebSocket(`ws://${location.host}/socket`);
   webSocket.onmessage = (event)=> {
-    console.debug('event', event.data);
+    dispatch({type: 'UPDATED_PARAMS', data: JSON.parse(event.data)});
   };
   webSocket.onopen = ()=> {
-    webSocket.send('hello!');
-    console.debug('open and sent hello');
+    webSocket.send('START');
   };
   webSocket.onclose = ()=> {
-    console.debug('closed');
   };
+}
+
+function reducer(state, {type, data}) {
+  if (!state) {
+    return {};
+  }
+  if (type === 'UPDATED_PARAMS') {
+    return data;
+  }
+
+  return state;
+}
+
+function MonitoringBar({param, minValue, maxValue}, state) {
+  const value = state[param];
+  const percent = Math.max(
+    Math.min(
+      ((value - minValue) / (maxValue - minValue)) * 100,
+      100),
+    1
+  );
+  return {
+    innerText: value.toFixed(2),
+    style: {
+      width: `${percent}%`,
+    },
+  };
+}
+
+const COMPONENTS = {
+  '[data-param="ph"] div':
+    MonitoringBar.bind(null, {param: 'ph', minValue: 4, maxValue: 7}),
+  '[data-param="color"] div':
+    MonitoringBar.bind(null, {param: 'color', minValue: 0, maxValue: 60}),
+  '[data-param="bitterness"] div':
+    MonitoringBar.bind(null, {param: 'bitterness', minValue: 0, maxValue: 80}),
+  '[data-param="turbidity"] div':
+    MonitoringBar.bind(null, {param: 'turbidity', minValue: 0, maxValue: 100}),
+  '[data-param="alcohol"] div':
+    MonitoringBar.bind(null, {param: 'alcohol', minValue: 0, maxValue: 20}),
+  '[data-param="alcohol"] div':
+    MonitoringBar.bind(null, {param: 'alcohol', minValue: 0, maxValue: 20}),
+  '[data-param="attenuation"] div':
+    MonitoringBar.bind(null, {param: 'attenuation', minValue: 0, maxValue: 100}),
+  '[data-param="gravity"] div':
+    MonitoringBar.bind(null, {param: 'gravity', minValue: 1, maxValue: 1.2}),
+};
+
+
+function updateAttributes(element, attributes) {
+  Object.entries(attributes).forEach(([attribute, value])=> {
+    if (value && typeof value === 'object') {
+      updateAttributes(element[attribute], value);
+    } else {
+      element[attribute] = value;
+    }
+  });
+
+}
+function renderComponent(Component, element, state) {
+  const result = Component(state);
+  updateAttributes(element, Component(state));
+}
+
+function render(state) {
+  Object.entries(COMPONENTS).forEach(([selector, Component])=> {
+    const element = document.querySelector(selector);
+    element && renderComponent(Component, element, state);
+  });
+}
+
+function main() {
+  let state = reducer(null, {type: 'INIT'});
+  render(state);
+  socket((action)=> {
+    state = reducer(state, action);
+    render(state);
+  });
 }
 
 main()
